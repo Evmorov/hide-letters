bedc1396bc5684516e7fd0f088515c28b00870c2
Evgeny Morozov <evmorov@gmail.com>
2015-06-22 19:28:23 +0100

    first commit

---
+++ hide_letters.js
@@ -0,0 +1,47 @@
+"use strict"
+
+// Returns a random integer between min (included) and max (excluded)
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+String.prototype.replaceAt = function(index, character) {
+  return this.substr(0, index) + character + this.substr(index+character.length);
+}
+
+function hide(str) {
+  var len = str.length;
+  var open = 0;
+
+  if (len <= 5) {
+    open = 0;
+  } else if (5 < len && len <= 10) {
+    open = 1;
+  } else if (10 < len && len <= 15) {
+    open = 2;
+  } else if (15 < len && len <= 20) {
+    open = 3;
+  } else if (20 < len){
+    open = 4;
+  }
+
+  var open_positions = [];
+  while (open > 0) {
+    var open_position = getRandomInt(0, len);
+    if (str.charAt(open_position) == ' ') continue;
+    if (open_positions.indexOf(open_position) != -1) continue;
+    open_positions.push(open_position);
+    open--;
+  }
+
+  for (var i = 0; i < len; i++) {
+    if (open_positions.indexOf(i) != -1) continue;
+    if (str.charAt(i) == ' ') continue;
+    str = str.replaceAt(i, '_');
+  }
+
+  if (typeof document !== "undefined") {
+    document.getElementById("result").value = str
+  }
+  return str;
+}
---
+++ hide_letters_test.js
@@ -0,0 +1,50 @@
+var assert = require('chai').assert;
+var fs = require('fs');
+var vm = require('vm');
+var path = './hide_letters.js';
+var code = fs.readFileSync(path);
+vm.runInThisContext(code);
+
+function wo_space_and_underscore(str) {
+  return str.replace(/[_ ]/g, '')
+}
+
+it('len <= 5', function() {
+  assert.equal(hide('abcde'), '_____');
+})
+
+it('5 < len <= 10', function() {
+  assert.lengthOf(wo_space_and_underscore(hide('abcdefghij')), 1);
+})
+
+it('5 < len <= 10 with spaces', function() {
+  assert.lengthOf(wo_space_and_underscore(hide('abc def gh')), 1);
+})
+
+it('10 < len <= 15', function() {
+  assert.lengthOf(wo_space_and_underscore(hide('abcdefghijklmno')), 2);
+})
+
+it('15 < len <= 20', function() {
+  assert.lengthOf(wo_space_and_underscore(hide('abcdefghijklmnopqrst')), 3);
+})
+
+it('len > 20', function() {
+  assert.lengthOf(wo_space_and_underscore(hide('abcdefghijklmnopqrstabcdefghijklmnopqrst')), 4);
+})
+
+it('len < 5 with spaces', function() {
+  assert.equal(hide('a c e'), '_ _ _');
+})
+
+it('len > 5 with spaces', function() {
+  assert.lengthOf(wo_space_and_underscore(hide('a c e f')), 1);
+})
+
+it('number of letters', function() {
+  assert.lengthOf(hide('test string test'), 16);
+})
+
+it('other symbols', function() {
+  assert.equal(hide('!?-.,'), '_____');
+})
